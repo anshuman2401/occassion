d982b3224cd0111079110fc99b5c578da81010a5
Avijeet Gaikwad <avijeet7@users.noreply.github.com>
2021-01-28 22:45:09 +0530

    Cart page (#7)
    * cart page
    
    * fixed multiple items, skuid
    
    Co-authored-by: Avijeet <avijeet.gaikwad@myntra.com>

--- cart.js
+++ cart.js
@@ -1,39 +1,55 @@
 let occassion = ["Wedding", "Reception", "Marriage", "Festival", "Office", "Beach", "Dining", "Romantic", "Sports"];
 
-let cartDiv = document.createElement("div");
-cartDiv.style = "display: inline-flex;width: -webkit-fill-available;overflow: scroll;height: 110px;";
+function createCartDiv(cartId) {
+  let cartDiv = document.createElement("div");
+  cartDiv.id = "cart_div_" + cartId;
+  cartDiv.style = "display: inline-flex;width: -webkit-fill-available;overflow: scroll;height: 110px;";
 
-for (let i = 0; i < 9; i++) {
-  let slide = document.createElement("div");
-  slide.id = "slide_" + i;
+  for (let i = 0; i < 9; i++) {
+    let slide = document.createElement("div");
+    slide.id = "slide_" + i;
 
-  let image = document.createElement("img");
-  image.id = "slide_image_" + i;
-  image.style = "width: 140px;padding: 10px;filter: blur(1px);";
-  image.src = chrome.extension.getURL("images/static_image_" + i + ".jpg");
+    let image = document.createElement("img");
+    image.id = "cart_" + cartId + "_slide_image_" + i;
+    image.style = "width: 140px;padding: 10px;filter: blur(1px);";
+    image.src = chrome.extension.getURL("images/static_image_" + i + ".jpg");
 
-  let ratings = document.createElement("div");
-  ratings.style = "position: relative;top: -35px;left: 20px;font-size: 15px;color: white;text-shadow: 1px 1px black;";
-  ratings.innerHTML = occassion[i];
+    let ratings = document.createElement("div");
+    ratings.style = "position: relative;top: -35px;left: 20px;font-size: 15px;color: white;text-shadow: 1px 1px black;";
+    ratings.innerHTML = occassion[i];
 
-  slide.appendChild(image);
-  slide.appendChild(ratings);
+    slide.appendChild(image);
+    slide.appendChild(ratings);
 
-  slide.addEventListener("click", () => slideClicked(i));
+    slide.addEventListener("click", () => slideClicked(cartId, i));
 
-  cartDiv.appendChild(slide);
-}
-
-function slideClicked(i) {
-  document.getElementById("slide_image_" + i).style = "width: 140px;padding: 10px;filter: blur(0px);border: 2px solid #555;";
-  for (let j = 0; j < 9; j++) {
-    if (j != i) document.getElementById("slide_image_" + j).style = "width: 140px;padding: 10px;filter: blur(1px);";
+    cartDiv.appendChild(slide);
   }
-  console.log(i);
+  return cartDiv;
 }
 
-let buyingForText = document.createElement("div");
-buyingForText.innerHTML = "Buying for occasion:";
-document.getElementsByClassName("itemContainer-base-details")[0].appendChild(buyingForText);
+function slideClicked(cartId, i) {
+  document.getElementById("cart_" + cartId + "_slide_image_" + i).style =
+    "width: 140px;padding: 10px;filter: blur(0px);border: 2px solid #555;";
+  for (let j = 0; j < 9; j++) {
+    if (j != i)
+      document.getElementById("cart_" + cartId + "_slide_image_" + j).style =
+        "width: 140px;padding: 10px;filter: blur(1px);";
+  }
+  let urlString = document.getElementsByClassName("itemContainer-base-itemLink")[cartId].href;
+  var skuId = new URLSearchParams(urlString).get("skuId");
+  console.log(skuId, i);
+}
 
-document.getElementsByClassName("itemContainer-base-details")[0].appendChild(cartDiv);
+function buyingForText(bId) {
+  let buyingForText = document.createElement("div");
+  buyingForText.id = "buyingForText_" + bId;
+  buyingForText.innerHTML = "Buying for occasion:";
+  return buyingForText;
+}
+
+let itemsNo = document.getElementsByClassName("itemContainer-base-details").length;
+for (let i = 0; i < itemsNo; i++) {
+  document.getElementsByClassName("itemContainer-base-details")[i].appendChild(buyingForText(i));
+  document.getElementsByClassName("itemContainer-base-details")[i].appendChild(createCartDiv(i));
+}
